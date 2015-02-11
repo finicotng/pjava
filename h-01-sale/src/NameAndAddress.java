2e98d84702c4831c50a828314c9407f476df2067
finico <finico.a@gmail.com>
2015-02-11 23:32:07 +0300

    homework 01 sale

---
+++ h-01-sale/src/NameAndAddress.java
@@ -0,0 +1,8 @@
+public class NameAndAddress {
+    public static void main(String[] args) {
+        System.out.println("John Doe");
+        System.out.println("Some Street");
+        System.out.println("Some City");
+        System.out.println("Some Country");
+    }
+}
